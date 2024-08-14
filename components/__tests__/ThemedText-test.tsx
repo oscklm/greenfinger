3796c650cad89f6c33d40af584dccd77458861c5
oscklm <oscar@oneguylabs.com>
2024-08-14 18:14:09 +0200

    chore: setup yarn 3.8.4 and migrate to @testing-library/react-native

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
-
+import { render } from '@testing-library/react-native';
 import { ThemedText } from '../ThemedText';
 
 it(`renders correctly`, () => {
-  const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
+  const { toJSON, getByText } = render(<ThemedText>Snapshot test!</ThemedText>);
+  
+  expect(getByText('Snapshot test!')).toBeTruthy(); // Ensuring the text is rendered
+  expect(toJSON()).toMatchSnapshot();
 });
